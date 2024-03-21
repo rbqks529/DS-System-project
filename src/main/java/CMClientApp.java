f5ef2d65932db98f8315a1c8f4eea78a1487195c
rbqks529 <rbqks529@naver.com>
2024-03-21 11:07:11 +0900

    TestCode

--- src/main/java/CMClientApp.java
+++ src/main/java/CMClientApp.java
@@ -1,25 +1,35 @@
+
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 
 import java.util.Scanner;
 
+/**
+ * A simple CM client application
+ * <h1>Note</h1>
+ * Before you run this client, you must run a server (CMServerApp).
+ * <br>Check that the SERVER_ADDR field of the cm-client.conf file is the same as that of
+ * the cm-server.conf file.
+ * <br>After you run this client,
+ * <br>1. the client starts CM and logs in to the server with the user name "ccslab".
+ * <br>2. If you press the enter key, CM and the client terminates.
+ */
 public class CMClientApp {
     private CMClientStub m_clientStub;
     private CMClientEventHandler m_eventHandler;
-
-    public CMClientApp(){
+    public CMClientApp() {
         m_clientStub = new CMClientStub();
         m_eventHandler = new CMClientEventHandler(m_clientStub);
     }
 
-    public CMClientStub getClientStub(){
+    public CMClientStub getClientStub() {
         return m_clientStub;
     }
 
-    public CMClientEventHandler getClientEventHandler(){
+    public CMClientEventHandler getClientEventHandler() {
         return m_eventHandler;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         CMClientApp client = new CMClientApp();
@@ -27,32 +37,35 @@ public class CMClientApp {
         CMClientEventHandler eventHandler = client.getClientEventHandler();
         boolean ret = false;
 
-        //initalize CM
+        // initialize CM
         clientStub.setAppEventHandler(eventHandler);
         ret = clientStub.startCM();
 
-        if(ret){
-            System.out.println("init success");
-        } else {
-            System.err.println("init error!");
+        if(ret)
+            System.out.println("CM initialization succeeds.");
+        else {
+            System.err.println("CM initialization error!");
             return;
         }
 
-        //login CM server
+        // login CM server
+        System.out.println("=== login: ");
         System.out.println("user name: ccslab");
         System.out.println("password: ccslab");
         ret = clientStub.loginCM("ccslab", "ccslab");
 
-        if(ret){
+        if(ret)
             System.out.println("successfully sent the login request.");
-        } else {
+        else {
             System.err.println("failed the login request!");
             return;
         }
 
-        //wait before excuting next API
-        System.out.println("Press enter to excute net API:");
+        // terminate CM
+        System.out.println("Press enter to excute next API");
         scanner.nextLine();
 
+        scanner.close();
     }
+
 }
--- src/main/java/CMServerApp.java
+++ src/main/java/CMServerApp.java
@@ -1,26 +1,51 @@
 import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
 
+import java.util.Scanner;
+
+/**
+ * A simple CM server application.
+ * <h1>Note</h1>
+ * Before you run this server, check SERVER_ADDR field of cm-server.conf.
+ * <br>After you run this server, the server starts CM.
+ * <br>Then, you can run a client application.
+ */
 public class CMServerApp {
     private CMServerStub m_serverStub;
     private CMServerEventHandler m_eventHandler;
 
-    public CMServerApp(){
+    public CMServerApp() {
         m_serverStub = new CMServerStub();
         m_eventHandler = new CMServerEventHandler(m_serverStub);
     }
 
-    public  CMServerStub getServerStub(){
+    public CMServerStub getServerStub() {
         return m_serverStub;
     }
 
-    public CMServerEventHandler getServerEventHandler(){
+    public CMServerEventHandler getServerEventHandler() {
         return m_eventHandler;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         CMServerApp server = new CMServerApp();
-        CMServerStub cmStub = server.getServerStub();
-        cmStub.setAppEventHandler(server.getServerEventHandler());
-        cmStub.startCM();
+        CMServerStub serverStub = server.getServerStub();
+        serverStub.setAppEventHandler(server.getServerEventHandler());
+        boolean ret = false;
+
+        // start CM
+        ret = serverStub.startCM();
+
+        if(ret) {
+            System.out.println("CM initialization succeeds.");
+        }
+        else {
+            System.err.println("CM initialization error!");
+        }
+
+        // terminate CM
+        System.out.println("Enter to terminate CM and server: ");
+        scanner.nextLine();
+        serverStub.terminateCM();
     }
 }
