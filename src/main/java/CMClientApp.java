33a84da6490d36ffc5eb6d60f44bbe1df3edbb96
JoGyuBin <rbqls3529@gmail.com>
2024-03-26 01:01:03 +0900

    client GUI complete

--- src/main/java/CMClientApp.java
+++ src/main/java/CMClientApp.java
@@ -1,12 +1,20 @@
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.*;
 
 public class CMClientApp {
     private CMClientStub m_clientStub;
     private CMClientEventHandler m_eventHandler;
+    private JTextPane consoleTextPane = new JTextPane();
+    private JButton loginButton = new JButton("Login");
+    private JButton logoutButton = new JButton("Logout");
+
 
     public class DrawingPanel extends JPanel {
         private String shapes; // 그려진 도형들을 문자열로 저장
@@ -20,22 +28,29 @@ public class CMClientApp {
             shapes = ""; // 초기화
             currentShape = "line"; // 기본 도형은 선
 
-            addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    xBegin = e.getX();
-                    yBegin = e.getY();
+            addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mousePressed(java.awt.event.MouseEvent evt) {
+                    xBegin = evt.getX();
+                    yBegin = evt.getY();
                 }
 
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                    xEnd = e.getX();
-                    yEnd = e.getY();
+                public void mouseReleased(java.awt.event.MouseEvent evt) {
+                    xEnd = evt.getX();
+                    yEnd = evt.getY();
                     // 새로운 도형을 shapes에 추가하고 다시 그리기
                     shapes += String.format("%s,%d,%d,%d,%d;", currentShape, xBegin, yBegin, xEnd, yEnd);
                     repaint();
                 }
+            });
 
+            addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+                public void mouseDragged(java.awt.event.MouseEvent evt) {
+                    xEnd = evt.getX();
+                    yEnd = evt.getY();
+
+                    // 현재 도형을 shapes에 추가하고 다시 그리기
+                    repaint();
+                }
             });
 
             // 도형 선택 버튼 리스너 설정
@@ -100,6 +115,7 @@ public class CMClientApp {
     public CMClientApp() {
         m_clientStub = new CMClientStub();
         m_eventHandler = new CMClientEventHandler(m_clientStub);
+        createWhiteboard();
     }
 
     public CMClientStub getClientStub() {
@@ -110,83 +126,183 @@ public class CMClientApp {
         return m_eventHandler;
     }
 
-    private void displayLoginDialog() {
-        JFrame frame = new JFrame("CM Login");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 150);
-        frame.setLayout(new GridLayout(3, 2));
+    public void printMessage(String strText)
+    {
+        StyledDocument doc = consoleTextPane.getStyledDocument();
+        try {
+            doc.insertString(doc.getLength(), strText, null);
+            consoleTextPane.setCaretPosition(consoleTextPane.getDocument().getLength());
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
 
-        JLabel lbUsername = new JLabel("Username: ");
-        JTextField tfUsername = new JTextField();
-        JLabel lbPassword = new JLabel("Password: ");
-        JPasswordField pfPassword = new JPasswordField();
-        JButton btnLogin = new JButton("Login");
-        JButton btnCancel = new JButton("Cancel");
+        return;
+    }
+    public void printStyledMessage(String strText, String strStyleName)
+    {
+        StyledDocument doc = consoleTextPane.getStyledDocument();
+        try {
+            doc.insertString(doc.getLength(), strText, doc.getStyle(strStyleName));
+            consoleTextPane.setCaretPosition(consoleTextPane.getDocument().getLength());
 
-        frame.add(lbUsername);
-        frame.add(tfUsername);
-        frame.add(lbPassword);
-        frame.add(pfPassword);
-        frame.add(btnLogin);
-        frame.add(btnCancel);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
 
-        // 버튼 액션 리스너
-        btnLogin.addActionListener(new ActionListener() {
+    private void createWhiteboard() {
+        // 공유 화이트 보드 프레임 생성
+        JFrame whiteboardFrame = new JFrame("공유 화이트 보드");
+        whiteboardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        whiteboardFrame.setLayout(new BorderLayout());
+        whiteboardFrame.setSize(1000, 800);
+
+        // 도형 그리는 패널 생성
+        DrawingPanel drawingPanel = new DrawingPanel();
+        whiteboardFrame.add(drawingPanel, BorderLayout.CENTER);
+
+        // 클래스 멤버 변수로 설정
+        consoleTextPane.setBackground(new Color(245, 245, 245));
+        consoleTextPane.setEditable(false); // 편집 불가능하도록 설정
+
+        // JTextPane을 JScrollPane으로 감싸기
+        JScrollPane scrollPane = new JScrollPane(consoleTextPane);
+        // JScrollPane의 preferredSize를 설정하여 크기 늘리기
+        Dimension preferredSize = new Dimension(300, 200); // 예시로 너비 300, 높이 200 설정
+        scrollPane.setPreferredSize(preferredSize);
+        // JScrollPane을 whiteboardFrame에 추가
+        whiteboardFrame.add(scrollPane, BorderLayout.SOUTH);
+
+        // 로그인 및 로그아웃 버튼 패널 생성
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT)); // 오른쪽 정렬 설정
+
+        logoutButton.setPreferredSize(new Dimension(90, 30));
+        logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String username = tfUsername.getText();
-                String password = new String(pfPassword.getPassword());
-
-                // CM에 로그인 시도
-                boolean ret = m_clientStub.loginCM(username, password);
-                // 로그인 버튼 액션 내부에서 로그인 성공 시 처리
-                if (ret) {
-                    // 로그인 성공 메시지 창
-                    JOptionPane.showMessageDialog(frame, "Login successful.");
-                    frame.dispose(); // 로그인 창 닫기
-
-                    // 공유 화이트 보드 프레임을 생성 및 표시하는 코드
-                    SwingUtilities.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            // 공유 화이트 보드 프레임 생성
-                            JFrame whiteboardFrame = new JFrame("공유 화이트 보드");
-                            whiteboardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            whiteboardFrame.setSize(1000, 800);
-
-                            // 도형 그리는 패널 생성
-                            DrawingPanel drawingPanel = new DrawingPanel();
-                            whiteboardFrame.add(drawingPanel, BorderLayout.CENTER);
-
-                            // 콘솔을 표시하는 패널 생성
-                            JPanel consolePanel = new JPanel();
-                            JTextArea consoleTextArea = new JTextArea(10, 70);
-                            consoleTextArea.setEditable(true); // 콘솔 편집 불가능하게 설정
-                            JScrollPane scrollPane = new JScrollPane(consoleTextArea);
-                            consolePanel.add(scrollPane);
-                            whiteboardFrame.add(consolePanel, BorderLayout.SOUTH);
-
-                            // 프레임 위치 설정 및 표시
-                            whiteboardFrame.setLocationRelativeTo(null); // 화면 중앙에 위치
-                            whiteboardFrame.setVisible(true);
-                        }
-                    });
-                } else {
-                    // 로그인 실패 메시지 창
-                    JOptionPane.showMessageDialog(frame, "Login failed.", "Error", JOptionPane.ERROR_MESSAGE);
-                }
+                // 로그아웃 메서드 호출
+                logout();
             }
         });
 
-        // 취소 버튼 액션
-        btnCancel.addActionListener(new ActionListener() {
+        loginButton.setPreferredSize(new Dimension(70, 30));
+        loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.dispose();
+                // 로그인 창 표시
+                login();
             }
         });
-        frame.setLocationRelativeTo(null); // 창을 화면 중앙에 배치
-        frame.setVisible(true);
+
+        buttonPanel.add(loginButton);
+        buttonPanel.add(logoutButton);
+        whiteboardFrame.add(buttonPanel, BorderLayout.NORTH);
+
+        // 프레임 위치 설정 및 표시
+        whiteboardFrame.setLocationRelativeTo(null); // 화면 중앙에 위치
+        whiteboardFrame.setVisible(true);
+    }
+    private void login()
+    {
+        String strUserName = null;
+        String strPassword = null;
+        boolean bRequestResult = false;
+
+        printMessage("====== login to default server\n");
+        JTextField userNameField = new JTextField();
+        JPasswordField passwordField = new JPasswordField();
+        Object[] message = {
+                "User Name:", userNameField,
+                "Password:", passwordField
+        };
+        int option = JOptionPane.showConfirmDialog(null, message, "Login Input", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION)
+        {
+            strUserName = userNameField.getText();
+            strPassword = new String(passwordField.getPassword()); // security problem?
+
+            m_eventHandler.setStartTime(System.currentTimeMillis());
+            bRequestResult = m_clientStub.loginCM(strUserName, strPassword);
+            long lDelay = System.currentTimeMillis() - m_eventHandler.getStartTime();
+            if(bRequestResult)
+            {
+                printMessage("successfully sent the login request.\n");
+                printMessage("return delay: "+lDelay+" ms.\n");
+            }
+            else
+            {
+                printStyledMessage("failed the login request!\n", "bold");
+                m_eventHandler.setStartTime(0);
+            }
+        }
+        printMessage("======\n");
+
+        int nClientState;
+        nClientState = m_clientStub.getCMInfo().getInteractionInfo().getMyself().getState();
+
+        switch(nClientState)
+        {
+            case CMInfo.CM_INIT: //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);
+                break;
+            case CMInfo.CM_CONNECT: //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);;
+                break;
+            case CMInfo.CM_LOGIN: //로그아웃 버튼
+                loginButton.setEnabled(false);
+                logoutButton.setEnabled(true);
+                break;
+            case CMInfo.CM_SESSION_JOIN:    //로그아웃 버튼
+                loginButton.setEnabled(false);
+                logoutButton.setEnabled(true);
+                break;
+            default:    //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);
+                break;
+        }
+    }
+
+    private void logout() {
+        boolean bRequestResult = false;
+        printMessage("====== logout from default server\n");
+        bRequestResult = m_clientStub.logoutCM();
+        if (bRequestResult)
+            printMessage("successfully sent the logout request.\n");
+        else
+            printStyledMessage("failed the logout request!\n", "bold");
+        printMessage("======\n");
+
+        int nClientState;
+        nClientState = m_clientStub.getCMInfo().getInteractionInfo().getMyself().getState();
+
+        switch(nClientState)
+        {
+            case CMInfo.CM_INIT: //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);
+                break;
+            case CMInfo.CM_CONNECT: //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);;
+                break;
+            case CMInfo.CM_LOGIN: //로그아웃 버튼
+                loginButton.setEnabled(false);
+                logoutButton.setEnabled(true);
+                break;
+            case CMInfo.CM_SESSION_JOIN:    //로그아웃 버튼
+                loginButton.setEnabled(false);
+                logoutButton.setEnabled(true);
+                break;
+            default:    //로그인 버튼
+                loginButton.setEnabled(true);
+                logoutButton.setEnabled(false);
+                break;
+        }
     }
 
     public static void main(String[] args) {
@@ -202,12 +318,5 @@ public class CMClientApp {
             return;
         }
         System.out.println("CM initialization succeeds.");
-        // GUI 로그인 창 표시
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                client.displayLoginDialog();
-            }
-        });
     }
 }
